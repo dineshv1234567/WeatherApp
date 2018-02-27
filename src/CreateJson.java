fa2bab462c88c4222159e94ce54325ccdc9175d3
dinesh <dineshverma3008@gmail.com>
2018-02-28 01:37:23 +0530

    First commit

---
+++ src/CreateJson.java
@@ -0,0 +1,28 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+
+public class CreateJson {
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    JSONObject obj = new JSONObject();
+    obj.put("name", "neha");
+    obj.put("location", "sangrur");
+    
+    //JSONArray....add
+    
+    try(FileWriter fwrite=new FileWriter("myFile.json",true)) 
+    {
+    	fwrite.write(obj.toString());
+    	fwrite.flush();
+    } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    System.out.println(obj);
+	}
+
+}
